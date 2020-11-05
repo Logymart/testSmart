8379a17adf8e3cfc9f515345435defe94f71d5f4
LgmcSmart <lgm306@live.fr>
2020-11-05 13:31:26 +0000

    Creation de gitignore

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 </head>
 
 <body>
+	<h1>Git Organisation VCS </h1>
+	<h2>Versioning Control System</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt eius reiciendis doloremque rerum repellat! Maxime eum, vel, consectetur asperiores perferendis odit nam quasi voluptate deleniti illum laboriosam ea consequatur aperiam.</p>
 </body>
 </html>
